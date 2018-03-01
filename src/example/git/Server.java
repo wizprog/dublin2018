70e3a93a36d1a6711b7964bf7bf47a46ef5ddd6f
Marko Babic <mbabic96@gmail.com>
2018-03-01 15:25:27 +0100

    Molim te komituj me

---
+++ src/example/git/Server.java
@@ -0,0 +1,18 @@
+package example.git;
+
+public class Server {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Prvi primer");
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
